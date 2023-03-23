addcc5efa6ad981251c7bcdf35491bff593d3bf5
Maxime Maillot <maillot.maxime62@gmail.com>
2023-03-23 10:32:15 +0100

    add template of projet

---
+++ src/Components/Projet/ProjetList/ProjetList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./ProjetList.scss";
+
+const ProjetList = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ProjetList;
---
+++ src/Views/ProjetView/ProjetView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./ProjetView.scss";
+
+const ProjetView = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ProjetView;
