6ee56b9cdae335cea3b093df9ff00790d75f532c
Maxime Maillot <maillot.maxime62@gmail.com>
2023-03-27 10:22:15 +0200

    add footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.scss";
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <div>Me contacter</div>
+            <div>Maxime Maillot</div>
+            <div>CV React (lien github)</div>
+        </div>
+    );
+};
+
+export default Footer;
